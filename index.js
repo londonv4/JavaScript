70d98ec7b88d19a309d9e4ecea5f69db363293f2
londonv4 <londonv4@nycstudents.net>
2026-03-30 12:32:15 -0400

    ok

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-Document.queryselector(“.btn”)
-Console.log(button)
-Const box= document.queryselector(“.box”);
-button.addEventListener(“click”, function (event) {
+const sushiOrders = [
+  { name: "California Roll", price: 8 },
+  { name: "Spicy Tuna Roll", price: 10 },
+  { name: "Salmon Nigiri", price: 6 },
+  { name: "California Roll", price: 8 },
+  { name: "Dragon Roll", price: 12 },
+  { name: "Spicy Tuna Roll", price: 10 },
+  { name: "Miso Soup", price: 4 },
+  { name: "Edamame", price: 5 },
+  { name: "Salmon Nigiri", price: 6 },
+  { name: "California Roll", price: 8 },
+];
 
-let r = Math.floor(Math.random() * 256));
-let g = Math.floor(Math.random() * 256));
-let b = Math.floor(Math.random() *256));
-
-box.style.backgroundColor = ‘rgb(${r}, ${g},  ${b})’;
-}
-button.addEventListener(“Click”, MAKERGB);
-
-
+const receipt = {};
+sushiOrders.forEach((sushi) => {
+    if (!receipt[sushi.name]) {
+        receipt[sushi.name] = {
+         price: sushi.price,
+         qty:1,
+        };
+    } else {
+      receipt[sushi.name].qty++;
+    }
 });
